022d7b706ec83e1eb9c4d640348186d77279c793
Valentina Banner <78287030+bannev1@users.noreply.github.com>
2025-07-17 11:46:30 +0800

    structure settings

--- settingsManager.py
+++ settingsManager.py
@@ -5,46 +5,26 @@ from dotenv import load_dotenv
 from Helpers import Database, Prompter
 
 
-# SETTINGS OBJECT REPRESENTATION
-class Settings:
-    def __init__(self, settings: dict) -> None:
-        """
-        Set settings. Also makes sure all values set, otherwise Key_Error
+# Export
+def exportSettings(settings: dict, exportPath: str = "./settings.json") -> None:
+    """
+    Export settings to JSON file as a dictionary
 
-        Args:
-            settings (dict): Settings
-        """
+    Args:
+        exportPath (str): Path to export JSON file to
+    """
 
-        # Set attributes
-        self.settings = settings
-        self.serverName = settings['Server_Name']
-        self.serverDescription
-    
-
-    def export(self, exportPath: str = "./settings.json") -> dict:
-        """
-        Export settings to JSON file as a dictionary
-
-        Args:
-            exportPath (str): Path to export JSON file to
-        """
-
-        # Get settings
-        settings = self.settings
-
-        # Open and export
-        with open(exportPath, 'r+') as file:
-            json.dump(settings, file)
-
-        return settings
+    # Open and export
+    with open(exportPath, 'r+') as file:
+        json.dump(settings, file)
 
 
-
-# PROCESSORS
-
-def settingsFromDB(envPath: str = 'env_data.env') -> Settings:
+# Get settings from database directly (without descriptions)
+def settingsFromDB(envPath: str = 'env_data.env') -> dict:
     """
     Generate settings from database directly with AI
+    
+    Note descriptions will be set as an empty string, and will have to be manually explained.
 
     Args:
         envPath (str): Environment variables .env file path to process connection string
@@ -62,9 +42,11 @@ def settingsFromDB(envPath: str = 'env_data.env') -> Settings:
         }
     )
 
-def settingsFromDB(connection: dict[str]) -> Settings:
+def settingsFromDB(connection: dict[str]) -> dict:
     """
-    Generate settings from database directly with AI
+    Generate settings from database directly with AI. 
+    
+    Note descriptions will be set as an empty string, and will have to be manually explained.
 
     Args:
         connection (dict[str]): Connection properties. See README for exact structure
@@ -74,14 +56,150 @@ def settingsFromDB(connection: dict[str]) -> Settings:
     db = Database(connection)
 
     # Get all tables
-    db.execute("""
+    tableNames = db.execute("SELECT table_name FROM dba_tables;")
 
-""")
+    tables = []
+
+    # Get all fields
+    for tableName in tableNames:
+        table = {
+            'Name': tableName,
+            'Description': ''
+        }
+
+        layout = []
+
+        fields = db.execute(f"SELECT column_name, data_type FROM all_tab_cols WHERE table_name = '{tableName}';")[0]
+
+        for field in fields:
+            struct = {
+                'Name': field['column_name'],
+                'Type': field['data_type'],
+                'Description': ''
+            }
 
 
-def parseSettings(settingsPath: str) -> Settings:
+        tables.append(table)
+
+
+# AI Settings
+def settingsWithAI(details: str, envPath: str) -> dict:
+    """
+    Use AI to generate the settings dictionary, given details about database
+
+    Args:
+        details (str): String explaining database layout
+    """
+
+    # Load environment variables
+    load_dotenv(envPath)
+
+    # Process
+    return settingsWithAI(
+        details,
+        {
+            "AZURE_OPENAI_API_KEY": os.getenv('AZURE_OPENAI_API_KEY'),
+            "AZURE_OPENAI_ENDPOINT": os.getenv('AZURE_OPENAI_ENDPOINT'),
+            "AZURE_OPENAI_API_VERSION": os.getenv('AZURE_OPENAI_API_VERSION'),
+            "AZURE_OPENAI_DEPLOYMENT_NAME": os.getenv('AZURE_OPENAI_DEPLOYMENT_NAME'),
+            "AZURE_AI_KEY": os.getenv('AZURE_AI_KEY'),
+            "AZURE_AI_ENDPOINT": os.getenv('AZURE_AI_ENDPOINT')
+        }
+    )
+
+def settingsWithAI(details: str, apiKeys: dict) -> dict:
+    """
+    Use AI to generate the settings dictionary, given details about database
+
+    Args:
+        details (str): String explaining database layout
+    """
+
+    settingsTemplate = json.dumps(EXAMPLE_SETTINGS, indent='\t') # Get settings layout for AI
+
+    ai = Prompter(apiKeys)
+
+    basePrompt = f"Given a description of the database, you should output a JSON file (make sure to not output anything else) similar to the following template for their database.\n\nTemplate: \n{settingsTemplate}"
+
+    result = json.loads(ai.prompt(basePrompt, details))
+
+    return result
+
+
+# Parse JSON
+def parseSettings(settingsPath: str) -> dict:
     """
     Parse JSON path to get settings
+
+    Args:
+        settingsPath (str): Path of settings/JSON file
     """
 
-    return Settings(json.loads(settingsPath))
+    return json.loads(settingsPath)
+
+
+
+
+# EXAMPLE SETTINGS/TEMPLATE
+EXAMPLE_SETTINGS = {
+    "Server_Name": "My Server",
+    "Server_Description": "Brief Server Description",
+    "SQL_Flavor": "Oracle Database",
+    "Tables": [
+        {
+            "Name": "Table 1 Name",
+            "Description": "Table 1 Description",
+            "Layout": [
+                {
+                    "Name": "Field 1 Name",
+                    "Type": "String",
+                    "Description": "Explanation of what field 1 is for/contains",
+                    "Properties": {
+                        "isPrimaryKey": True,
+                        "Foreign_Reference": "",
+                        "Constraints": "NOT NULL"
+                    }
+                },
+
+                {
+                    "Name": "Field 2 Name",
+                    "Type": "Boolean",
+                    "Description": "Explanation of what field 2 is for/contains",
+                    "Properties": {
+                        "isPrimaryKey": False,
+                        "Foreign_Reference": "Table 2 Name",
+                        "Constraints": ""
+                    }
+                }
+            ]
+        },
+
+        {
+            "Name": "Table 2 Name",
+            "Description": "Table 2 Description",
+            "Layout": [
+                {
+                    "Name": "Field 1 Name",
+                    "Type": "Integer",
+                    "Description": "Explanation of what field 1 is for/contains",
+                    "Properties": {
+                        "isPrimaryKey": True,
+                        "Foreign_Reference": "",
+                        "Constraints": "NOT NULL"
+                    }
+                },
+
+                {
+                    "Name": "Field 2 Name",
+                    "Type": "VARCHAR2(50)",
+                    "Description": "Explanation of what field 2 is for/contains",
+                    "Properties": {
+                        "isPrimaryKey": False,
+                        "Foreign_Reference": "",
+                        "Constraints": ""
+                    }
+                }
+            ]
+        }
+    ]
+}
